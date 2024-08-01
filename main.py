35be7e117d1ff19e84e26741663d718cef3c31ab
RhysMarch <c1012902@newcastle.ac.uk>
2024-08-01 14:06:10 +0100

    Feat: posts recent announcements

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-CHANNEL_ID = 1264365566907650128  # Ensure this is the correct channel ID
+CHANNEL_ID = 1264365566907650128
 
 intents = discord.Intents.default()
-intents.message_content = True  # Enable message content intent
+intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-last_update = ""  # Initialize to empty string for initial update
-message_sent = None  # Variable to track the sent message
+last_update = ""
+message_sent = None
 
 
 @tasks.loop(minutes=1)  # Check for updates every minute
@@ -26,22 +26,22 @@ async def check_metro_updates():
 
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for bad responses
+        response.raise_for_status()
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Use a more specific selector to find the target div
+
         updates_div = soup.find('div', class_='views-row')
         if updates_div:
             current_update = updates_div.get_text(separator='\n', strip=True)
 
-            if current_update != last_update:  # Check for changes
-                last_update = current_update  # Update last_update
+            if current_update != last_update:
+                last_update = current_update
                 channel = bot.get_channel(CHANNEL_ID)
 
                 if channel:
-                    if message_sent:  # Edit existing message if available
+                    if message_sent:
                         await message_sent.edit(content=f"**Nexus Metro Updates:**\n{current_update}")
-                    else:  # Send new message if none exists
+                    else:
                         message_sent = await channel.send(f"**Nexus Metro Updates:**\n{current_update}")
 
                     print(f"Sent Metro update: {current_update}")
@@ -53,7 +53,7 @@ async def check_metro_updates():
 
 
 @bot.event
-async def on_ready():  # Send initial message when the bot is ready
+async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     check_metro_updates.start()  # Start the update task
 
